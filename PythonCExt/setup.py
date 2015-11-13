265cc967de3c460852bc8cf450e1b13092bf35fb
Richard Saunders <rts@brhazrts.rinconres.com>
2015-11-13 11:24:29 -0700

    Upates to Ptools 1.6.1

--- PythonCExt/setup.py
+++ PythonCExt/setup.py
@@ -5,13 +5,13 @@ setup(name="pyobjconvert", version="1.0",
       ext_modules=[
     Extension("pyobjconvertmodule",
               ["pyobjconvertmodule.cc", "pyobjconverter.cc"],
-              include_dirs=['../C++', '../C++/opencontainers_1_8_1/include']),
+              include_dirs=['../C++', '../C++/opencontainers_1_8_2/include']),
     ])
 
 setup(name="ocserialization", version="1.0",
       ext_modules=[
     Extension("pyocsermodule",
               ["pyocsermodule.cc", "pyocser.cc"],
-              include_dirs=['../C++', '../C++/opencontainers_1_8_1/include']),
+              include_dirs=['../C++', '../C++/opencontainers_1_8_2/include']),
     ])
 
